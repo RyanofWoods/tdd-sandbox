6321e9e62d96909fe90c2dffcd05b11dd27aabac
Ryan Woods <contact@ryanofwoods.com>
2021-04-08 14:14:32 +0200

    add capitalize function with tests

--- js/__tests__/string.jest.js
+++ js/__tests__/string.jest.js
@@ -2,4 +2,22 @@ import { capitalize } from '../string';
 // toBe, toEqual, toBeNull, toBeUndefined, toBeDefined, toBeTruthy, toBeFalsy, toMatch
 
 //CAPITALIZE
-
+test('Capitalize should handle null', () => {
+  expect(capitalize(null)).toBe(null);
+});
+test('Capitalize should handle an empty string', () => {
+  expect(capitalize('')).toEqual('');
+});
+test('Capitalize should capitalize an lowercase word', () => {
+  expect(capitalize('ryan')).toEqual('Ryan');
+});
+test('Capitalize should format an uppercase word correctly', () => {
+  expect(capitalize('RYAN')).toEqual('Ryan');
+});
+test('Capitalize should format an mixed case word correctly', () => {
+  expect(capitalize('rYAn')).toEqual('Ryan');
+});
+test('Capitalize should handle one letter words', () => {
+  expect(capitalize('a')).toEqual('A');
+  expect(capitalize('A')).toEqual('A');
+});
--- js/string.js
+++ js/string.js
@@ -1,5 +1,7 @@
 // creating functions to practice TDD, even if the method is built into js
 
 export function capitalize(word) {
-  
+  if (!word) return word;
+
+  return word[0].toUpperCase() + word.substring(1).toLowerCase();
 }
